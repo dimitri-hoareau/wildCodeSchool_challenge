83af0397d3abcff23fc045c950bf834d1c0a69af
dimitri-hoareau <dimitrihoareau2@gmail.com>
2020-07-18 12:12:09 +0200

    integration de la vue ejs

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const router = require('./app/router');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.set('views', 'app/views');
+
 app.use(express.static(__dirname + '/public'));
 
 app.use(router);
